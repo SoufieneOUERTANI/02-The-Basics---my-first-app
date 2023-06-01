bbe2fd554c52f2d43f89b8f29c853ae794558183
Soufiene OUERTANI <soufiene.ouertani@gmail.com>
2023-06-01 09:47:04 +0200

    21 - Using template straight in Typescript class

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  template: 
+  `<app-server></app-server>
+  <h1>Template dans le classe TypeScript</h1>
+  <app-server></app-server>`
+  ,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
