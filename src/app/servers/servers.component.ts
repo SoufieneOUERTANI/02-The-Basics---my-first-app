145066126fb87ff453457e027234b856f2e73d18
Soufiene OUERTANI <soufiene.ouertani@gmail.com>
2023-06-02 08:38:33 +0200

    27 - Outpunt binding [property] = "data"

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,13 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: '.app-servers',
-  template: 
-  `<app-server></app-server>
-  <h1>Template dans le classe TypeScript</h1>
-  <app-server></app-server>`
-  ,
+  // template: 
+  // `<app-server></app-server>
+  // <h1>Template dans le classe TypeScript</h1>
+  // <app-server></app-server>`
+  // ,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+
+  allowNewServer = false;
+
+  constructor(){
+    setTimeout(() => this.allowNewServer=true, 2000);
+  }
 
 }
