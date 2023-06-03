6ec8be82bace337321e9c0ad7d558f4ea4f3ddb0
Soufiene OUERTANI <soufiene.ouertani@gmail.com>
2023-06-03 23:40:59 +0200

    39 - [ngStyle] directive to change the style

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,10 +9,17 @@ export class ServerComponent{
     serveId :number = 10;
     serverStatus : string = "offline"
 
-    message : string = "Game Over"
-
-    getServerStatus (){
-        return this.message;
+    constructor(){
+        this.serverStatus=Math.random()>0.5?"online":"offline";
     }
 
+    // message : string = "Game Over"
+
+    getServerStatus (){
+        return this.serverStatus;
+    }
+
+    getColour() {
+        return this.serverStatus=="online"?"green":"red";
+    }
 }
