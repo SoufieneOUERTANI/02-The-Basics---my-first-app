cd09a252c6211512739be7fbff0463f062e0b5d5
Soufiene OUERTANI <soufiene.ouertani@gmail.com>
2023-11-17 08:02:29 +0100

    27. Property Binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,13 +8,21 @@ import { Component } from '@angular/core';
   // selector: '[app-servers]',
   // -- select by element
   // selector: 'app-servers',
-  template: 
-  `<app-server></app-server>
-  <h1>Template dans le classe TypeScript</h1>
-  <app-server></app-server>`
-  ,
+  templateUrl : './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+
+  allowNewServer : boolean = false;
+
+  constructor(){
+    setTimeout(()=> {
+      this.allowNewServer= true;
+    } , 3000);
+  }
+
+  allowNewServerMethod(){
+    return !this.allowNewServer;
+  }
 
 }
